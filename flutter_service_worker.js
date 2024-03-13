00e1367725c86360ed965a8ee6047b024dd9a0ae
MaksLachugin <maks.lachugin@mail.ru>
2024-03-13 23:18:11 +0300

    Built web
    Branch: master
    Commit: 250be78caf1638654156fd6217ba26fb7ff0c5af
    
    package:peanut 5.0.0

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -7,7 +7,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "assets/AssetManifest.bin.json": "69a99f98c8b1fb8111c5fb961769fcd8",
 "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
-"assets/fonts/MaterialIcons-Regular.otf": "b973801a304b851feb1d768178a57274",
+"assets/fonts/MaterialIcons-Regular.otf": "108e526acae67c261248d7fd8ab2b07c",
 "assets/NOTICES": "ecae372be791f17a36df94de88f1a0ef",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "89ed8f4e49bcdfc0b5bfc9b24591e347",
 "assets/shaders/ink_sparkle.frag": "4096b5150bac93c41cbc9b45276bd90f",
@@ -24,11 +24,11 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "aa36e3ebad91515fe21fa8ae8054d6fb",
-"/": "aa36e3ebad91515fe21fa8ae8054d6fb",
-"main.dart.js": "76c3418322aa5e10a7837ab5d37bda55",
+"index.html": "bcc1115fc1f2872df386f49a2b4438c7",
+"/": "bcc1115fc1f2872df386f49a2b4438c7",
+"main.dart.js": "264e0d3d6623600e1c0bb3bcd0346a93",
 "manifest.json": "da6ad9055eb6bf41f4fc73e0d02c97de",
-"version.json": "e133e20762acae7b3bbc1dd09641c760"};
+"version.json": "06997d7f61c7574252ea56c984b83f79"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
